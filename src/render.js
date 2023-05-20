a1813cca67d4598daf012634838e3babddef667e
Artur Imankulov <artur.imankulov906@gmail.com>
2023-05-20 22:21:33 +0500

    fix

--- src/render.js
+++ src/render.js
@@ -3,7 +3,6 @@ const RenderPosition = {
     AFTERBEGIN: 'afterbegin',
     BEFOREEND: 'beforeend',
     AFTEREND: 'afterend',
-<<<<<<< HEAD
   };
   
   function createElement(template) {
@@ -18,18 +17,3 @@ const RenderPosition = {
   }
   
   export { RenderPosition, createElement, render };
-=======
-};
-  
-function createElement(template) {
-    const newElement = document.createElement('div');
-    newElement.innerHTML = template;
-    return newElement.firstElementChild;
-}
-  
-function render(component, container, place = RenderPosition.BEFOREEND) {
-    container.insertAdjacentElement(place, component.getElement());
-}
-  
-export { RenderPosition, createElement, render };
->>>>>>> 21766160992a6645f7c083a1ed5f72f728623d48
